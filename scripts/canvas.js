33e1d8b0e2b0df39195275df0dffae79463e204b
Masashi.i <masashi.itamura.spt90@gmail.com>
2024-02-08 19:29:45 +0900

    add interactive table

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,35 +1,49 @@
 function showWindowSize() {
-  var width = window.innerWidth;
-  var height = window.innerHeight;
+  let width = window.innerWidth;
+  let height = window.innerHeight;
   document.getElementById('resize-check').textContent = width + ' x ' + height;
 }
 
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+let lastTime = 0; // Variable to store the last time the circles were drawn
+const interval = 2000; // Time step in milliseconds
+let timestampCheck = document.querySelector('#timestamp-check');
 
 function resizeCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  drawCircles();
 }
 
 function drawCircle() {
-    var radius = Math.floor(Math.random() * 40) + 10; // Random radius between 10 and 50
-    var x = Math.floor(Math.random() * canvas.width);
-    var y = Math.floor(Math.random() * canvas.height);
-    var color = '#' + Math.floor(Math.random()*16777215).toString(16); // Random color
+  let radius = Math.floor(Math.random() * 40) + 10; // Random radius between 10 and 50
+  // let x = Math.floor(Math.random() * canvas.width);
+  // let y = Math.floor(Math.random() * canvas.height);
+  let x = canvas.width / 2;
+  let y = canvas.height / 2;
+  let z = Math.floor(Math.random() * 2);
+  let color = '#' + Math.floor(Math.random()*16777215).toString(16); // Random color
 
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = color;
-    ctx.fill();
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+  ctx.fillStyle = color;
+  ctx.fill();
 }
 
-function drawCircles() {
-  for (var i = 0; i < 100; i++) {
-    drawCircle();
+function drawCircles(timestamp) {
+  if (!lastTime) lastTime = timestamp;
+  const elapsed = timestamp - lastTime;
+
+  if (elapsed > interval) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < 5; i++) {
+      drawCircle();
+    }
+    lastTime = timestamp;
   }
+  window.requestAnimationFrame(drawCircles);
 }
+
 
 window.addEventListener('resize', function() {
   console.log('resize');
@@ -40,4 +54,4 @@ window.addEventListener('resize', function() {
 // initial setup
 showWindowSize();
 resizeCanvas();
-drawCircles();
+window.requestAnimationFrame(drawCircles);
---
+++ scripts/language_learning.js
@@ -0,0 +1,46 @@
+const wordsData = [
+  { german: 'ich', english: 'I' },
+  { german: 'du', english: 'you' },
+  { german: 'er', english: 'he' },
+  { german: 'sie', english: 'she' },
+  { german: 'es', english: 'it' },
+]
+
+const wordPairHTML = generateWordPairHTML();
+
+
+function generateWordPairHTML(wordsData) {
+  return wordsData.map((wordPair, index) => `
+    <tr>
+      <td>${wordPair.german}</td>
+      <td>${wordPair.english}</td>
+      <td><button class="delete-button" data-index="${index}">X</button></td>
+    </tr>
+  `).join('');
+}
+
+function updateTable() {
+  const tableBody = document.getElementById('table-body');
+  tableBody.innerHTML = generateWordPairHTML(wordsData);
+}
+
+updateTable();
+
+const addButton = document.getElementById('add-button');
+
+addButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  const englishInput = document.getElementById('english-input').value;
+  const germanInput = document.getElementById('german-input').value;
+  if (englishInput && germanInput) {
+    wordsData.push({
+      english: englishInput,
+      german: germanInput,
+    });
+  updateTable();
+  document.getElementById('english-input').value = '';
+  document.getElementById('german-input').value = '';
+  } else {
+    alert('Please fill in both fields');
+  }
+} );
