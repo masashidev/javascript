69d9e46f0af5ab6afc4ddf37f1328a65bd88510a
Masashi.i <masashi.itamura.spt90@gmail.com>
2024-02-13 15:46:31 +0900

    add pasteImage function at pointer position

--- scripts/normal_drag.js
+++ scripts/normal_drag.js
@@ -59,14 +59,12 @@ container2.appendChild(circle);
 
 // get the position of the mouse
 function getMousePosition(event) {
-  console.log("mouse position: ",event.clientX, event.clientY);
   const circleWidth = circle.offsetHeight;
   const circleHeight = circle.offsetWidth;
   let x = event.clientX - circleWidth / 2;
   let y = event.clientY - circleHeight / 2;
   circle.style.left = x + 'px';
   circle.style.top = y + 'px';
-  console.log("circle position: ",x, y);
 }
 
 
@@ -81,9 +79,26 @@ window.onload = function() {
 
   // Function to update the image position based on the mouse position
   function updateImagePosition(event) {
-  img.style.left = event.pageX + 'px';
-  img.style.top = event.pageY + 'px';
+    const pointerWidth = img.offsetWidth;
+    const pointerHeight = img.offsetHeight;
+    img.style.left = event.pageX - pointerWidth / 2 + 'px';
+    img.style.top = event.pageY - pointerHeight / 2 + 'px';
+
   }
 
   document.addEventListener('mousemove', updateImagePosition)
+
 }
+
+function pasteImage(event) {
+  let img = document.createElement('img');
+  img.src = '/assets/images/pointer.png';
+  img.classList.add('pointer-image');
+  const x = event.clientX - 50;
+  const y = event.clientY - 50;
+  img.style.left = x + 'px';
+  img.style.top = y + 'px';
+  document.body.appendChild(img);
+}
+
+document.addEventListener('click', pasteImage);
