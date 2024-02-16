722b463563d05ffc4ca61acf4e841943e5701061
Masashi.i <masashi.itamura.spt90@gmail.com>
2024-02-16 09:32:49 +0900

    add mobile event handler

--- scripts/mobile_view.js
+++ scripts/mobile_view.js
@@ -5,8 +5,11 @@ console.log(  'Hello from mobile_view.js' )
 // global variables
 
 // state variables
+  // canvas entities
 
 // utility functions
+  // Draw a circle
+
 
 //dom elements
 const canvas = document.querySelector('canvas');
@@ -18,15 +21,56 @@ canvas.style.backgroundColor = '#000000';
 
 //event listeners
 function setupEventListeners() {
-  canvas.addEventListener('mousedown', handleMouseDown);
-  canvas.addEventListener('mousemove', handleMouseMove);
-  canvas.addEventListener('mouseup', handleMouseUp);
+  canvas.addEventListener('pointerdown', handlePinterDown);
+  canvas.addEventListener('pointermove', handlePointerMove);
+  canvas.addEventListener('pointerup', handlePointerup);
 }
 
-// event handler functions
-function handleMouseDown(event) {
-  console.log('mousedown');
+// object classes
+class Circle {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
 }
+// physical objects
+  // constructor
+
+  //gravity method
+
+  //update method
+
+  //draw method
+
+
+
+// event handler functions
+function handlePinterDown(event) {
+  console.log('pointerdown');
+}
+
+function handlePointerMove(event) {
+  console.log('pointermove');
+}
+
+function handlePointerup(event) {
+  console.log('pointerup');
+}
+
+// animation variables
+// animation function
+
+
+// instances variables
+
 
 // initialization
 function init() {
